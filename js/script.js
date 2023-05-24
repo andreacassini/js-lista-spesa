7b82a08eb01ee3f5ab0a5228bfb1e26d5e26ba32
andreacassini <andrea1316@hotmail.it>
2023-05-24 18:09:34 +0200

    ADD output list

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ let listaSpesa = ['Zucchero', 'Lievito', 'Cacao', 'Sale', 'Cioccolato fondente',
 const lista = document.getElementById('lista-spesa');
 let i = 0;
 while(i < listaSpesa.length){
-    console.log(listaSpesa[i]);
+    let item = listaSpesa[i]
     i++;
+    let list_item = document.createElement('li');
+    list_item.innerText =  item;
+    lista.append(list_item);
 }
 
